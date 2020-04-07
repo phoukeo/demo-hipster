cebb5ce4cc64f86b6ae0402ad1b89a624fa46bad
PT Thilavanh <pt@ignw.io>
2020-04-07 15:00:12 -0700

    update jenkinsfile

--- hipster-all-deploy/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export NAMESPACE=hipster
-#deploy application
-kubectl create namespace ${NAMESPACE}
-kubectl apply -f ./ -n ${NAMESPACE}
--- hipster-all-deploy/uninstall.sh
+++
@@ -1,5 +0,0 @@
-# Set namespace to desired name
-export NAMESPACE="hipster"
-
-kubectl delete -n ${NAMESPACE} -f ./
-kubectl delete namespace ${NAMESPACE}
